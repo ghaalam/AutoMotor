f31631fc290ad4a828b0f9264c0887fa8aefaac9
l <davidzarate538@gmail.com>
2019-04-23 20:43:37 -0500

    Primercodigo

---
+++ src/asociacionautomotor/Motor.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asociacionautomotor;
+
+/**
+ *
+ * @author CRACK USER
+ */
+public class Motor 
+{
+private String Modelo;
+private String NumSerie;
+private String Combustible;
+private int CabaFuerza;
+private int Cilindros;
+
+}
